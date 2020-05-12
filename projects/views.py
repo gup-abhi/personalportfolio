de36eb85769a441c93c5e924554a9f3eb090c445
Abhishek Gupta <agabhishek20121998@gmail.com>
2020-05-12 17:16:40 +0530

    initial commit

---
+++ projects/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+
+
+# Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects' : projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    context = {
+        'project' : project
+    }
+    return render(request, 'project_detail.html', context)
